774992efd0b0ce0070afe22c5523bb229178c160
Samii Shabuse <sshabuse@gmail.com>
2025-10-28 00:54:13 -0400

    binary impl

--- src/main/java/edu/drexel/se320/BinarySearch.java
+++ src/main/java/edu/drexel/se320/BinarySearch.java
@@ -1,5 +1,5 @@
 package edu.drexel.se320;
-
+import java.util.NoSuchElementException;
 public class BinarySearch {
 
     // DO NOT MODIFY THIS SIGNATURE
@@ -7,9 +7,30 @@ public class BinarySearch {
     // on a combination of overloading and visibility hacks to swap out your
     // code at runtime to test your test suite.
     protected static <T extends Comparable<T>> int binarySearchImplementation(T[] array, T elem) {
-	// TODO: Copy over your HW1 implementation of binary search here
-        if (elem == null) return -1;
-        return elem.compareTo(elem);
-    }
+        
+        // Input validation
+        if (array == null) throw new IllegalArgumentException("array");
+        if (elem == null) throw new IllegalArgumentException("elem");
+        if (array.length == 0) throw new IllegalArgumentException("Input array is empty");
 
+        // Binary search implementation
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int compare =  elem.compareTo(array[mid]);
+
+            if  (compare == 0) {
+                return mid; // Element was found
+            }
+            else if (compare < 0) {
+                right = mid - 1; // Search the left half
+            }
+            else {
+                left = mid + 1; // search the right half
+            }
+        }
+        throw new NoSuchElementException("Element was not in array: " + elem);
+    }
 }
